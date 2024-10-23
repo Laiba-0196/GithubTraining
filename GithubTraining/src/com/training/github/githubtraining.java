fe963f68a57db0cff9eb37e10526aec866445d8c
Laiba Mukhtar <124293768+Laiba-0196@users.noreply.github.com>
2024-10-23 09:21:12 +0500

    Update githubtraining.java

--- GithubTraining/src/com/training/github/githubtraining.java
+++ GithubTraining/src/com/training/github/githubtraining.java
@@ -6,6 +6,7 @@ public class githubtraining {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Thsi is Doctor Math");
 	}
 
 }
